5cb4d48a12ab6c511e1e2e10191ffe0f8b3bdafa
shuoscaryo <shuoscaryo@gmail.com>
2023-10-06 17:06:47 +0200

    updated gitignore

---
+++ philo/include/philosophers.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 15:41:42 by orudek            #+#    #+#             */
+/*   Updated: 2023/10/06 16:29:23 by orudek           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <pthread.h>
+
+typedef struct s_data
+{
+	t_philo *philo;
+}	t_data;
+
+typedef struct s_philo
+{
+	
+}	t_philo;
+#endif
