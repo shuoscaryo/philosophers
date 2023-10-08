c33de3155f56665c33ac7827a1719b4a2a1451f5
shuoscaryo <shuoscaryo@gmail.com>
2023-10-08 19:26:56 +0200

    created data and philo structs, added program initialization

--- philo/include/philosophers.h
+++ philo/include/philosophers.h
@@ -6,22 +6,48 @@
 /*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 15:41:42 by orudek            #+#    #+#             */
-/*   Updated: 2023/10/06 16:29:23 by orudek           ###   ########.fr       */
+/*   Updated: 2023/10/08 19:24:38 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
+# include <stdio.h>
+# include <stdlib.h>
 # include <pthread.h>
+# include <sys/time.h>
 
-typedef struct s_data
-{
-	t_philo *philo;
-}	t_data;
+# define MAX_INT 2147483647
+
+typedef pthread_mutex_t	t_mtx;
 
 typedef struct s_philo
 {
-	
+	int		id;
+	int		state; //0 waiting to eat, 1 sleeping
+	int		last_eat_time;
+	int		meals_remaining;
+	int 	*time_to_eat;
+	int 	*time_to_sleep;
+	t_mtx	forks[2];
 }	t_philo;
+
+typedef struct s_data
+{
+	int 		time_to_die;
+	int 		time_to_eat;
+	int 		time_to_sleep;
+	int			num_of_meals;
+	int			num_of_philos;
+	t_mtx		write_lock;
+	t_mtx 		*forks;
+	pthread_t	*thread;
+	t_philo		*philo;
+}	t_data;
+
+int	save_inputs(t_data *data, int argc, char **argv);
+int	create_mutex(t_data *data);
+int	free_mutex(pthread_mutex_t *mutex, int len);
+
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,19 +6,31 @@
 /*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 15:37:58 by orudek            #+#    #+#             */
-/*   Updated: 2023/10/06 16:49:33 by orudek           ###   ########.fr       */
+/*   Updated: 2023/10/08 19:25:08 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	free_data(t_data *data, int exit_status)
+{
+	
+}
 
 int	main(int argc, char **argv)
 {
 	t_data	data;
+	int		i;
 
-	if (!init_data(&data, argc, argv))
+	if (argc != 5 && argc != 6)
 		return (1);
-	int i = -1;
-	while (++i < data->num_philos)
+	if (!save_inputs(&data, argc, argv))
+		return (1);
+	if (!create_mutex(&data))
+		return (1);
+	i = -1;
+	while (++i < data.num_of_philos)
 	{
-		pthread_create();
 	}
+	return (0);
 }
